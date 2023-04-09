470ee9cdf37997bdc751d3269b5f40054b14642c
curlew <git@curlew.is>
2023-04-09 17:55:57 +1000

    find module tag in multiboot2 info struct

--- kernel/boot/init.c
+++ kernel/boot/init.c
@@ -1,10 +1,21 @@
+#include <multiboot2.h>
 #include <serial.h>
 #include <stdint.h>
 
-void init(void) {
+void init(struct multiboot_tag *multiboot_info) {
     serial_init();
     serial_puts("hello world");
 
-    volatile uint16_t *const vga_buffer = (uint16_t *)0xB8000;
-    *vga_buffer = (15 << 8) | 'X';
+    for (multiboot_tag_t *tag = (multiboot_tag_t *)((uintptr_t)multiboot_info + 8); // skip fixed first tag
+         tag->type != MULTIBOOT_TAG_TYPE_END;
+         // align up to next 8-byte boundary
+         tag = (multiboot_tag_t *)((uintptr_t)tag + ((tag->size + 8-1) & ~(8-1)))) {
+        serial_puts("tag");
+        switch (tag->type) {
+        case MULTIBOOT_TAG_TYPE_MODULE:
+            serial_puts("found module tag. cmdline:");
+            serial_puts(((multiboot_tag_module_t *)tag)->cmdline);
+            break;
+        }
+    }
 }
--- kernel/multiboot2.h
+++ kernel/multiboot2.h
@@ -20,6 +20,29 @@
 
 #define MULTIBOOT_HEADER_TAG_OPTIONAL 1
 
+#define MULTIBOOT_TAG_TYPE_END              0
+#define MULTIBOOT_TAG_TYPE_CMDLINE          1
+#define MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME 2
+#define MULTIBOOT_TAG_TYPE_MODULE           3
+#define MULTIBOOT_TAG_TYPE_BASIC_MEMINFO    4
+#define MULTIBOOT_TAG_TYPE_BOOTDEV          5
+#define MULTIBOOT_TAG_TYPE_MMAP             6
+#define MULTIBOOT_TAG_TYPE_VBE              7
+#define MULTIBOOT_TAG_TYPE_FRAMEBUFFER      8
+#define MULTIBOOT_TAG_TYPE_ELF_SECTIONS     9
+#define MULTIBOOT_TAG_TYPE_APM              10
+#define MULTIBOOT_TAG_TYPE_EFI32            11
+#define MULTIBOOT_TAG_TYPE_EFI64            12
+#define MULTIBOOT_TAG_TYPE_SMBIOS           13
+#define MULTIBOOT_TAG_TYPE_ACPI_OLD         14
+#define MULTIBOOT_TAG_TYPE_ACPI_NEW         15
+#define MULTIBOOT_TAG_TYPE_NETWORK          16
+#define MULTIBOOT_TAG_TYPE_EFI_MMAP         17
+#define MULTIBOOT_TAG_TYPE_EFI_BS           18
+#define MULTIBOOT_TAG_TYPE_EFI32_IH         19
+#define MULTIBOOT_TAG_TYPE_EFI64_IH         20
+#define MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR   21
+
 #ifndef __ASSEMBLER__
 
 #include <stdint.h>
@@ -30,6 +53,20 @@ typedef struct multiboot_header {
     uint32_t header_length;
     uint32_t checksum;
 } multiboot_header_t;
+
+
+typedef struct multiboot_tag {
+    uint32_t type;
+    uint32_t size;
+} multiboot_tag_t;
+
+typedef struct multiboot_tag_module {
+    uint32_t type;
+    uint32_t size;
+    uint32_t mod_start;
+    uint32_t mod_end;
+    char cmdline[];
+} multiboot_tag_module_t;
 
 #endif // ifndef __ASSEMBLER__
 
